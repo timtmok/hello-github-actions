020f53b109325fae8b96cd5b284a6e7d6d667c25
Tim Mok <tim.mok@untappd.com>
2021-01-14 14:43:18 -0500

    Use octokit

---
+++ action-release/lib/main.js
@@ -0,0 +1,23 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+    function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+    function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { context } = require('@actions/github/lib/utils');
+function run() {
+  return __awaiter(this, void 0, void 0, function* () {
+    const octokit = github.getOctokit(core.getInput('github_token'))
+    octokit.repos.createRelease({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      tag_name: core.getInput('tag_name'),
+    })
+  });
+}
+run();
